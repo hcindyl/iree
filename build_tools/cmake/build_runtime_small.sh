b9846e54e07cc2ce283540cf048494ce34c2577a
Cindy Liu <hcindyl@google.com>
2023-02-21 18:21:36 -0800

    [POC] build bare-metal executable with static library workload

--- build_tools/cmake/build_runtime_small.sh
+++ build_tools/cmake/build_runtime_small.sh
@@ -16,15 +16,26 @@ BUILD_DIR="${1:-${IREE_RUNTIME_SMALL_BUILD_DIR:-build-runtime-small}}"
 source build_tools/cmake/setup_build.sh
 source build_tools/cmake/setup_ccache.sh
 
+IREE_OPTS="-DIREE_PLATFORM_GENERIC \
+  -DIREE_SYNCHRONIZATION_DISABLE_UNSAFE=1 \
+  -DIREE_FILE_IO_ENABLE=0 \
+  -DIREE_TIME_NOW_FN=\"{ return 0; }\""
+
 "${CMAKE_BIN?}" -B "${BUILD_DIR}" \
   -G Ninja . \
   -DPython3_EXECUTABLE="${IREE_PYTHON3_EXECUTABLE}" \
   -DPYTHON_EXECUTABLE="${IREE_PYTHON3_EXECUTABLE}" \
   -DCMAKE_BUILD_TYPE=MinSizeRel \
   -DIREE_SIZE_OPTIMIZED=ON \
-  -DIREE_BUILD_COMPILER=OFF
-"${CMAKE_BIN?}" --build "${BUILD_DIR}" -- -k 0
+  -DIREE_BUILD_COMPILER=OFF \
+  -DIREE_HOST_BIN_DIR=$(realpath build-host/install/bin) \
+  -DIREE_BUILD_SAMPLES=ON \
+  -DIREE_BUILD_TESTS=OFF \
+  -DCMAKE_SYSTEM_NAME=Generic \
+  -DIREE_ENABLE_THREADING=OFF \
+  -DIREE_HAL_DRIVER_DEFAULTS=OFF \
+  -DIREE_HAL_DRIVER_LOCAL_SYNC=ON \
+  -DIREE_HAL_EXECUTABLE_LOADER_DEFAULTS=OFF \
+  -DCMAKE_C_FLAGS="${IREE_OPTS}"
 
-if (( IREE_USE_CCACHE == 1 )); then
-  ccache --show-stats
-fi
+"${CMAKE_BIN?}" --build "${BUILD_DIR}" --target samples/static_library/all
